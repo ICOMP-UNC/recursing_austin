4799d46dcdcea98443fbb6362efdcde5aa96d927
saragervan <127273072+saragervan@users.noreply.github.com>
2024-11-15 17:41:06 -0300

    Add files via upload

---
+++ ED3ProyectoFinal/Debug/src/main.d
@@ -0,0 +1,11 @@
+src/main.o src/main.d: ../src/main.c \
+ C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h
+C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+C:\Users\SJ404\Documents\MCUXpressoIDE_24.9.25\workspace3\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
